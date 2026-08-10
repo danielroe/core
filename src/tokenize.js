843421b3ae982227cf65796d5f186bc9e6595004
Mathias <hi@mathias.ninja>
2026-08-10 17:35:14 +0200

    docs: add the missing import to the `tokenizeSync` example

--- src/tokenize.js
+++ src/tokenize.js
@@ -132,6 +132,7 @@ export function* tokenizer(src, lang, onToken) {
  * @example
  * import json from '@speed-highlight/core/languages/json.js';
  * import { tokenizeSync } from '@speed-highlight/core/tokenize';
+ * import process from 'node:process';
  *
  * tokenizeSync(src, json, (str, type) => process.stdout.write(str));
  *
